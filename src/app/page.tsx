fed3f25239a448cf6be7144e9fa6fe08eddaf1a8
Suraj <surajgupta.college@gmail.com>
2023-08-03 00:48:06 +0530

    added metaData

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,15 @@
+import { Metadata } from 'next'
 import DrawingBoard from "@/components/layouts/draw-board"
 import { DrawBoardFooter } from "@/components/layouts/draw-footer"
 import DrawBoardHeader from "@/components/layouts/draw-header"
+
+export const metadata: Metadata = {
+  title: 'PlayBoard',
+  description: 'Draw your creative thinking!',
+  verification: {
+    google: "5z2lDnQ6mdG9S2qZm74DNfOk3xdwLR-orzDHc5XiJxs"
+  }
+}
 
 export default function Home() {
   return (
