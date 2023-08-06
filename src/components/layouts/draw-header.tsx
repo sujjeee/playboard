f62cd21f4689c00cf1e784371b3a73b579eb03c8
Suraj <surajgupta.college@gmail.com>
2023-08-06 23:58:48 +0530

    fixed mobile bottom sheet

--- src/components/layouts/draw-header.tsx
+++ src/components/layouts/draw-header.tsx
@@ -1,11 +1,15 @@
+"use client"
+
 import React from 'react'
 import { Card } from "@/components/ui/card"
 import { ToolSettings } from '../tools/tool-settings'
 import ClearCanvasButton from '@/components/tools/clear-canvas'
 import TakeScreenShotButton from '@/components/tools/take-screenshot'
-import { MobileToolSettings } from '../tools/mobile-tool-settings'
+import { MobileToolSettings } from '@/components/tools/mobile-tool-settings'
+import useWindowSize from '@/hooks/useWindowSize'
 
 export default function DrawBoardHeader() {
+    const { isMobile, isDesktop } = useWindowSize();
     return (
         <div className="absolute top-5 w-full md:px-12 px-4">
             <div className=" flex justify-between items-center">
@@ -15,9 +19,8 @@ export default function DrawBoardHeader() {
                 <div className="items-center flex gap-3">
                     <ClearCanvasButton />
                     <TakeScreenShotButton />
-                    <ToolSettings />
-                    {/* under development */}
-                    {/* <MobileToolSettings /> */}
+                    {isDesktop && <ToolSettings />}
+                    {isMobile && <MobileToolSettings />}
                 </div>
             </div>
             <div>
