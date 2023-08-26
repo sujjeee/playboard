75a4967ab9003e29bd2c6a1980597cc51b713bff
Suraj <surajgupta.college@gmail.com>
2023-08-26 18:40:20 +0530

    fixed hiding tools error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,4 @@
-import { Metadata } from 'next'
 import DrawingBoard from "@/components/layouts/draw-board"
-
-export const metadata: Metadata = {
-  title: 'PlayBoard',
-  description: 'Draw your creative thinking!',
-  verification: {
-    google: "5z2lDnQ6mdG9S2qZm74DNfOk3xdwLR-orzDHc5XiJxs"
-  }
-}
 
 export default function Home() {
   return (
--- src/components/layouts/draw-header.tsx
+++ src/components/layouts/draw-header.tsx
@@ -6,23 +6,29 @@ import { ToolSettings } from '@/components/tools/tool-settings'
 import ClearCanvasButton from '@/components/tools/clear-canvas'
 import TakeScreenShotButton from '@/components/tools/take-screenshot'
 import { LiveCollab } from '../tools/live-collab'
+import { usePathname } from 'next/navigation'
 
 export default function DrawBoardHeader() {
+    const pathname = usePathname();
+
     return (
         <div className="absolute top-5 w-full md:px-12 px-4">
-            <div className=" flex justify-between items-center">
+            <div className="flex justify-between items-center">
                 <Card className="px-4 py-2 tracking-wide font-medium rounded-md select-none text-sm">
                     PlayBoard
                 </Card>
                 <div className="items-center flex gap-3">
+                    {/* todo: hide tools in not found page */}
+                    {/* {(pathname === '/' || pathname.startsWith("/room/")) && (
+                        <> */}
                     <ClearCanvasButton />
                     <TakeScreenShotButton />
                     <LiveCollab />
                     <ToolSettings />
+                    {/* </>
+                    )} */}
                 </div>
             </div>
-            <div>
-            </div>
         </div>
-    )
+    );
 }
